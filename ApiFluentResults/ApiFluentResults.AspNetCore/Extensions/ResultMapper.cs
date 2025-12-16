88d460da0c5fe4a0c77969220d577060200d83bb
ivan perez <ivansperezt@gmail.com>
2025-12-16 11:44:16 -0500

    feat: se elimina valores de la metadata cuando sea validacion de campos de entrada

--- ApiFluentResults/ApiFluentResults.AspNetCore/Extensions/ResultMapper.cs
+++ ApiFluentResults/ApiFluentResults.AspNetCore/Extensions/ResultMapper.cs
@@ -185,6 +185,7 @@ namespace Osom.FluentRestult.API.Extensions
             )
             {
                 problemDetails.Errors = validationErrors;
+                problemDetails.Metadata = null;
             }
 
             return problemDetails;
